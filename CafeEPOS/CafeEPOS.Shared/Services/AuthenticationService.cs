b7dab53752eba5b85adeeda1cbe6237bedf55379
Thomas Card <thomas.card@students.plymouth.ac.uk>
2024-11-20 21:21:36 +0000

    connect frontend app to api by adding code to auth service and linkin login page to the service

---
+++ CafeEPOS/CafeEPOS.Shared/Services/AuthenticationService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CafeEPOS.Shared.Services
+{
+    public class AuthenticationService
+    {
+        private string baseApiUrl = "http://localhost:5123/";
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<string> SystemAccountLoginCall(string email, string password)
+        {
+            var data = new
+            {
+                eamil = email,
+                password = password
+            };
+
+            var response = await _httpClient.PostAsJsonAsync(baseApiUrl + "Auth/SystemAccountLogin", data);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+    }
+}

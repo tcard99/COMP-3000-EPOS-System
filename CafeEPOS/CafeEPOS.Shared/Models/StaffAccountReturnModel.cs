22f784ab4948431a6bbef9c9023bfe5b728a3abb
Thomas Card <thomas.card@students.plymouth.ac.uk>
2025-04-18 13:27:26 +0100

    admin page works and can now see all staff for the cafe

---
+++ CafeEPOS/CafeEPOS.Shared/Models/StaffAccountReturnModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeEPOS.Shared.Models
+{
+    public class StaffAccountReturnModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string staffId { get; set; }
+        public string passcode { get; set; }
+        public int role { get; set; }
+        public int archived { get; set; }
+    }
+}
--- CafeEPOS/CafeEPOS.Shared/Services/AdminService.cs
+++ CafeEPOS/CafeEPOS.Shared/Services/AdminService.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CafeEPOS.Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +15,29 @@ namespace CafeEPOS.Shared.Services
 
         private static readonly HttpClient _httpClient = new HttpClient();
 
+        //Method to retive all staff accounts from the api
+        public async Task<List<StaffAccountReturnModel>> GetAllStaff(string sysAccountToken)
+        {
+            //Set up paramater
+            var param = $"sysAccountToken={sysAccountToken}";
 
+            //Set up request url
+            var url = $"{baseApiUrl}StaffManagment/GetAlStaff?{param}";
+
+            //Send request to API
+            var response = await _httpClient.GetAsync(url);
+
+            //Chcek to see if successful
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadFromJsonAsync<List<StaffAccountReturnModel>>();
+                return data;
+            }
+            else
+            {
+                return new List<StaffAccountReturnModel>();
+            }
+        }
 
     }
 }

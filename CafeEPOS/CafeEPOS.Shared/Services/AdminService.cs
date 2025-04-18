e78418ce61e9f4f6d6ef4150805f10baf44175aa
Thomas Card <thomas.card@students.plymouth.ac.uk>
2025-04-18 12:35:03 +0100

    added admin page and started service. all staff account end points have been created

---
+++ CafeEPOS/CafeEPOS.Shared/Services/AdminService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeEPOS.Shared.Services
+{
+    public class AdminService
+    {
+        private string baseApiUrl = "http://localhost:5123/";
+        //private string baseApiUrl = "https://web.socem.plymouth.ac.uk/comp3000/tcard-api/";
+
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+
+
+    }
+}

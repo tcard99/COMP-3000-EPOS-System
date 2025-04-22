1c546c0fdeaa4d6469d2d7934c7f70127a6be7cc
Thomas Card <thomas.card@students.plymouth.ac.uk>
2025-04-22 14:13:41 +0100

    stats controller and started stats page

---
+++ CafeEPOS/CafeEposAPI/Models/StatsReturnModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CafeEposAPI.Models
+{
+    public class StatsReturnModel
+    {
+        public decimal Sales { get; set; }
+        public int TotalOrders { get; set; }
+        public int OrdersPreparing { get; set; }
+        public decimal AvgPrepTime { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
